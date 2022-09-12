c02050d9e184fa7bd0527f31a4605be6c900252e
Cristian Leandro Perez Pelaez <clperez029@misena.edu.co>
2022-09-12 17:46:25 -0500

    Cambios en indentacion

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
-const selectedCharacters = document.getElementById("selectcharacters")
-const namecharacter = document.getElementById("nomCharacter")
-const imgcharacter = document.getElementById("imgCharacter")
-const speciecharacter = document.getElementById("specieCharacter")
-const genderCharacter = document.getElementById("genderCharacter")
+const selectedCharacters = document.getElementById("selectcharacters");
+const namecharacter = document.getElementById("nomCharacter");
+const imgcharacter = document.getElementById("imgCharacter");
+const speciecharacter = document.getElementById("specieCharacter");
+const genderCharacter = document.getElementById("genderCharacter");
 
-const api_rickandmorthy = 'https://rickandmortyapi.com/api'
+const api_rickandmorthy = "https://rickandmortyapi.com/api";
 
 console.log("selectedCharacters ----- > ", selectedCharacters.value);
 
-selectedCharacters.addEventListener('change', ()=>{
-    let actualcharacter = selectedCharacters.value
-    if (actualcharacter > 0){
-        fetch (`${api_rickandmorthy}/character/${actualcharacter}`,
-        {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-        cache: 'default'})
-        .then(res => res.json())
-        .then(data => {console.log(data)
-            namecharacter.textContent = data.name
-            imgcharacter.setAttribute('src', data.image)
-            speciecharacter.textContent = data.species
-            genderCharacter.textContent = data.gender
-        })
-    }
-})
+selectedCharacters.addEventListener("change", () => {
+  let actualcharacter = selectedCharacters.value;
+  if (actualcharacter > 0) {
+    fetch(`${api_rickandmorthy}/character/${actualcharacter}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      cache: "default",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        namecharacter.textContent = data.name;
+        imgcharacter.setAttribute("src", data.image);
+        speciecharacter.textContent = data.species;
+        genderCharacter.textContent = data.gender;
+      });
+  }
+});
